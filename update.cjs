77c22a8fca8c43aaa4f1308f90a7313d6e746a1b
oriionn <xemilien67@gmail.com>
2023-11-01 23:00:57 +0100

    feat: add GitHub Actions workflow to fetch data from Notion
    This commit adds a new GitHub Actions workflow file, `update.yml`, which is triggered manually. The workflow is responsible for fetching data from Notion and updating the repository with the latest data.
    
    The workflow uses the `ubuntu-latest` runner and consists of the following steps:
    - Checkout the repository
    - Install Node.js dependencies
    - Run a script (`update.cjs`) to fetch data from Notion using the Notion API
    - Add the fetched data (`competences.json` and `projects.json`) to the repository
    - Commit the changes with the message "Ajout des données depuis Notion"
    - Push the changes to the remote repository
    
    Additionally, this commit also includes the following changes:
    - Added `@notionhq/client` and `dotenv` as dependencies in `package.json`
    - Added `competences.json` and `projects.json` files, which are initially empty
    - Added `update.cjs` script, which fetches data from Notion and writes it to the respective JSON files

---
+++ update.cjs
@@ -0,0 +1,25 @@
+const { Client } = require('@notionhq/client');
+const fs = require("fs");
+// require("dotenv").config();
+
+const { NOTION_TOKEN, NOTION_DB_COMPETENCES, NOTION_DB_PROJECTS } = process.env;
+const notion = new Client({ auth: NOTION_TOKEN });
+
+(async () => {
+  let res = await notion.databases.query({ database_id: NOTION_DB_COMPETENCES });
+  let data = res.results.map((c) => {
+    return { name: c.properties["Nom"].title[0].text.content, percent: c.properties["Notation"].number }
+  })
+  fs.writeFileSync("competences.json", JSON.stringify(data, null, 2));
+
+  let resp = await notion.databases.query({ database_id: NOTION_DB_PROJECTS });
+  let data2 = resp.results.map((c) => {
+    return {
+      name: c.properties["Nom"].title[0].text.content,
+      description: c.properties["Description"].rich_text[0].text.content,
+      link: c.properties["Lien"].url,
+      image: c.properties["Logo"].url
+    }
+  });
+  fs.writeFileSync("projects.json", JSON.stringify(data2, null, 2));
+})();
